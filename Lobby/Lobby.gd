2603b896dc6250818d48fd1044290109dfc3b8fb
Jenny A <untmdsprt@gmail.com>
2019-08-31 22:30:19 -0500

    S2L10 - Populating the Waiting Room

--- Lobby/Lobby.gd
+++ Lobby/Lobby.gd
@@ -1,12 +1,32 @@
 extends Control
 
+onready var NameTextbox = $VBoxContainer/CenterContainer/GridContainer/NameTextBox
+onready var port = $VBoxContainer/CenterContainer/GridContainer/PortTextbox
+onready var selected_IP = $VBoxContainer/CenterContainer/GridContainer/IPTextbox
+
 func _ready():
-	$VBoxContainer/CenterContainer/GridContainer/NameTextBox.text = Saved.save_data["Player_name"]
+	NameTextbox.text = Saved.save_data["Player_name"]
+	selected_IP.text = Network.DEFAULT_IP
+	port.text = str(Network.DEFAULT_PORT)
 
 
 func _on_HostButton_pressed():
+	Network.selected_port = int(port.text)
 	Network.create_server()
+	create_waiting_room()
 
 
 func _on_JoinButton_pressed():
+	Network.selected_port = int(port.text)
+	Network.selected_IP = selected_IP.text
 	Network.connect_to_server()
+	create_waiting_room()
+
+
+func _on_NameTextBox_text_changed(new_text):
+	Saved.save_data["Player_name"] = NameTextbox.text
+	Saved.save_game()
+
+func create_waiting_room():
+	$WaitingRoom.popup_centered()
+	$WaitingRoom.refresh_players(Network.players)
